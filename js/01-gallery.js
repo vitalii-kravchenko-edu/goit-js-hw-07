b62036ac4a91b114dcc5b37e02122967f0004377
Vitalii Kravchenko <opabbk@gmail.com>
2023-11-04 17:37:06 +0200

    task1+extra

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+gallery.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+gallery.addEventListener("click", handleClick);
+
+function createMarkup(arr) {
+  return arr.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+}
+
+function handleClick(event) {
+  event.preventDefault();
+
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <div class="modal">  
+    <img src="${event.target.dataset.source}" >
+  </div>
+  `, {
+    onShow: () => window.addEventListener('keydown', onPress),
+    onClose: () => window.removeEventListener('keydown', onPress)
+  });
+
+  function onPress(e) {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  }
+
+  instance.show()
+}
